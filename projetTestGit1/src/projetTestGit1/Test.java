44b66471f369928028ac2b54053104faca25800b
utilisateur <utilisateur@DESKTOP-AD02GFS>
2020-02-10 11:47:03 +0100

    v4

--- projetTestGit1/src/projetTestGit1/Test.java
+++ projetTestGit1/src/projetTestGit1/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		System.out.println("Je suis test de Git");
 		System.out.println("Je suis test de Git v2");
 		System.out.println("Je suis test de Git v3");
+		System.out.println("Je suis test de Git v4");
 	}
 
 }
