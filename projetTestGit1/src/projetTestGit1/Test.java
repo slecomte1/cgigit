700a7a2b5f3600c009e9a824ad5efdfb46a62730
utilisateur <utilisateur@DESKTOP-AD02GFS>
2020-02-10 11:36:07 +0100

    v3

--- projetTestGit1/src/projetTestGit1/Test.java
+++ projetTestGit1/src/projetTestGit1/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Je suis test de Git");
 		System.out.println("Je suis test de Git v2");
+		System.out.println("Je suis test de Git v3");
 	}
 
 }
